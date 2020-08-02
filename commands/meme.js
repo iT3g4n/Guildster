9f862ab9d05dcbcb0338b330c94999c7984808e6
T3g4nTheCoder <winnerkiller966@gmail.com>
2020-08-02 12:46:36 +0300

    Update meme.js

--- commands/meme.js
+++ commands/meme.js
@@ -5,7 +5,7 @@ module.exports = {
     name: 'meme',
     description: "this is a meme command!",
     run: async (bot, message, args) => {
-        const subreddits = ["dankmeme", "meme", "dank_meme", "dankmemes", "memes", "me_irl"];
+        const subreddits = ["dankmeme", "meme", "dank_meme", "dankmemes", "memes", "me_irl", "facepalm"];
         const random = subreddits[Math.floor(Math.random() * subreddits.length)]
 
         const randomColor = Math.floor(Math.random() * 16777215).toString(16);
