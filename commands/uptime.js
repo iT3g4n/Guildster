e926bad448264a6fc32312453e8e74eb8e127bee
T3g4nTheCoder <winnerkiller966@gmail.com>
2020-08-02 12:11:30 +0300

    Update uptime.js

--- commands/uptime.js
+++ commands/uptime.js
@@ -5,6 +5,9 @@ module.exports= {
     description: "Tell you how long the bot has been online",
     usage: "*uptime",
     async run (bot, message, args) {
+
+        message.delete({ timeout: 0 })
+
         let m = await message.channel.send("Checking Uptime...")
 
         const e = new MessageEmbed()
