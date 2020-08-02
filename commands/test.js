570424d6a5db7c92e8585521a48b3e8080a31000
T3g4n <68778866+T3g4nTheCoder@users.noreply.github.com>
2020-08-02 19:16:31 +0300

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -5,20 +5,7 @@ module.exports = {
   name: 'test',
   description: "this is a test command!",
   async run(bot, message, args) {
-
-    const ms = require("ms"); // npm install ms
-
-    // g!start-giveaway 2d 1 Awesome prize!
-    // will create a giveaway with a duration of two days, with one winner and the prize will be "Awesome prize!"
-
-    bot.giveawaysManager.start(message.channel, {
-      time: ms(args[0]),
-      prize: args.slice(2).join(" "),
-      winnerCount: parseInt(args[1])
-    }).then((gData) => {
-      console.log(gData); // {...} (messageid, end date and more)
-    });
-    // And the giveaway has started!
+    message.channel.send("this is workin yo")
   }
 
 }
