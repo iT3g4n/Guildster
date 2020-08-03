4443de272ad67b326a62832fd91f9faaf6675f7f
T3g4nTheCoder <winnerkiller966@gmail.com>
2020-08-03 17:12:14 +0300

    Update ticket.js

--- commands/ticket.js
+++ commands/ticket.js
@@ -12,6 +12,8 @@ module.exports = {
 
         const msgArgs = args.slice().join(" ")
 
+        const mrname = message.author.username.slice(1).join(" ")
+
         const ticketembed = new MessageEmbed()
             .setTitle(`Suggestion by @${message.author.tag}`)
             .setDescription(`\n**Suggestion**\n${msgArgs}`)
@@ -20,7 +22,7 @@ module.exports = {
         const ticketid = "739480654109999185"
 
         bot.channels.cache.get(ticketid).send(ticketembed).then(
-            
+            yourchannel = bot.guilds.channels.find(channel => channel.name.startsWith(mrname) === "General")
         )
 
     }
