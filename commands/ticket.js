f76de0fa704dabf95678e5b0e8137b4626a07a69
T3g4nTheCoder <winnerkiller966@gmail.com>
2020-08-04 12:59:20 +0300

    Update ticket.js

--- commands/ticket.js
+++ commands/ticket.js
@@ -8,6 +8,8 @@ module.exports = {
     usage: "*ticket",
     async run(bot, message, args) {
 
+        if (message.channel.name != `${message.author.id}-ticket`) return;
+
         if (!args[ 1 && 2 && 3 && 4 && 5 && 6 && 7 && 8 && 9 ]) return message.channel.send("Please make the suggestion longer.")
 
         const msgArgs = args.slice().join(" ")
