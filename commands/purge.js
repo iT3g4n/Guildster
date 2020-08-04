8eb62a60d1ecf404a688ae44bc90f6e3c59c3cef
T3g4nTheCoder <winnerkiller966@gmail.com>
2020-08-04 13:02:17 +0300

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -3,6 +3,8 @@ module.exports = {
     description: "this is a purge command",
     execute(bot, message, args) {
 
+        message.delete({ timeout: 0 })
+
         if (message.member.roles.cache.has("728443375778529400") || (message.member.roles.cache.has("71628940296349302")) || (message.member.roles.cache.has("728443657140830299")) || (message.member.roles.cache.has("716238674089476116"))) {
 
         if (!args[0]) return message.channel.send("Please mention an amount to purge.").then(msg => {msg.delete({ timeout: 5000})})
