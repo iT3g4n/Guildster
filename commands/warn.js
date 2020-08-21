8fa9a0bde0d8602efd00e14dacd9a7c942a7f28b
T3g4nTheCoder <winnerkiller966@gmail.com>
2020-08-21 14:06:15 +0300

    Merge branch 'master' of https://github.com/T3g4nTheCoder/Discord-Bot

--- commands/warn.js
+++ commands/warn.js
@@ -1,9 +1,18 @@
 const discord = require("discord.js")
 const db = require("quick.db")
+const { Client, Message } = require('discord.js')
 
 module.exports = {
     name: 'warn',
     description: "this is a warn command!",
+
+    /**
+     * 
+     * @param {Client} bot 
+     * @param {Message} message
+     * @param {String[]} args 
+     */
+
     async run (bot, message, args) {
 
         if (message.member.permissions.has("MANAGE_MEMBERS")) {
@@ -13,27 +22,26 @@ module.exports = {
             if(!user) return message.reply("Please mention a valid user!")
             if(user.bot) return message.reply("You can't warn a bot!")
             if(message.author.id === user.id) return message.reply("You cant warn yourself!")
-            if(message.guild.owner.id === user.id) return message.reply("NO WARNING THE ALL POWERFUL GOD, T3G4N.")
+            if(message.guild.owner.id === user.id) return message.reply("NO WARNING THE SERVER OWNER.")
 
             let reason = args.slice(1).join(" ")
             if(!reason) return message.reply("Please give a reason.")
 
-            let warnings = db.get(`warnings_${message.guild.id}_${user.id}`);
+            let warnings = db.get(`warnings_${message.guild.id}_${user.id}_`);
             
             let warnEmbed = new discord.MessageEmbed()
-            .setTitle(`<@!${user.id}> has been warned`)
-            .setDescription(`Reason: ${reason}`)
+            .setDescription(`**<@${user.id}> has been warned**\n\nReason: ${reason}`)
             .setFooter(`Moderator ID: ${message.author.id}`)
-            .setColor('#ff7b00')
+            .setColor('ORANGE')
 
-            if (warnings = null || (warnings = 1) || (warnings = 2 || (warnings = 3))) {
-                db.add(`warnings_${message.guild.id}_${user.id}`, 1);
-                user.send(warnEmbed).catch(console.log(`could not send ${user.name || user.id} a dm. They must open them first.`))
-                await message.channel.send(`**${user.username || user.id}** has been warned for: ${reason}`)
+            if (warnings = null || (warnings = 1) || (warnings = 2) || (warnings = 3)) {
+                db.add(`warnings_${message.guild.id}_${user.id}_${reason}`, 1);
+                user.send(warnEmbed).catch(console.log(`could not send ${user.tag} a dm. They must open them first.`))
+                await message.channel.send(`**${user.tag}** has been warned for: ${reason}`).then(r => r.delete({ timeout: 3000 }))
                 bot.channels.cache.get("728653429785755730").send(warnEmbed);
             }
 
-            if (warnings === 3) return ("This person now has 3 warnings. Action: 2 hour mute")
+            if (warnings = 3) return ("This person now has 3 warnings. Action: 2 hour mute")
             
         } else {
 
