1bf72543293f4ced475edae1f7fe5ab6dc1548c1
T3g4n <68778866+T3g4nTheCoder@users.noreply.github.com>
2020-09-02 06:07:09 +0300

    Update guildRemove.js

--- events/guildRemove.js
+++ events/guildRemove.js
@@ -1,5 +1,6 @@
 const mongo = require("../mongo")
 const guildSchema = require(`../schemas/guildSchema`)
+const warnSchema = require(`../schemas/warnSchema`)
 const { Guild, Client } = require("discord.js")
 
 module.exports = {
@@ -12,6 +13,9 @@ module.exports = {
             try {
                 let data = await guildSchema.findOneAndRemove( {_id: guild.id} ).then()
                 console.log(`Guild Deleted:`, guild.name)
+
+                let moredata = await warnSchema.deleteMany({Guild: guild.id}).then()
+                
             } finally {
                 mongoose.connection.close()
             }
