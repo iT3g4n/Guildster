c70fec30c622fbb4d990986a7bbd8f6194b34898
T3g4n <68778866+T3g4nTheCoder@users.noreply.github.com>
2020-09-02 07:16:47 +0300

    Update dog.js

--- commands/dog.js
+++ commands/dog.js
@@ -9,19 +9,12 @@ module.exports = {
 
     message.delete({ timeout: 0 })
 
-    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
-
     let doggo = await message.channel.send("looking for a cute doggo...")
 
-    const subreddits = ["dog", "doggo", "dogs", "puppies", "pup"];
-    const random = subreddits[Math.floor(Math.random() * subreddits.length)]
-
-    const img = await rp(random)
+    const img = await rp();
     const pEmbed = new MessageEmbed()
-      .setTitle("Found one!" + " From /r/" + random)
-      .setURL(`https://reddit.com/r/${random}`)
       .setImage(img)
-      .setColor(`#${randomColor}`)
+      .setColor('RANDOM');
 
     await doggo.edit(pEmbed);
     doggo.edit("");
