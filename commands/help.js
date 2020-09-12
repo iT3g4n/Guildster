356ffc35dc39cad1bb377b408f418c2d592e40d8
T3g4n <68778866+T3g4nTheCoder@users.noreply.github.com>
2020-09-12 08:43:36 +0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,14 +1,6 @@
 const { MessageEmbed, Client, Message } = require("discord.js");
 const { helpEmbed } = require(`../index`)
 const fs = require(`fs`)
-let i = 1
-
-fs.readdir('./', (err, files) => {
-  if (err) return console.error(err);
-  files.forEach(file => {
-    i++
-  });
-});
 
 module.exports = {
   name: 'Help',
@@ -22,13 +14,15 @@ module.exports = {
   run: async (bot, message, args) => {
 
     if (!args[0]) {
-      embed = helpEmbed.setColor('RANDOM').setThumbnail(message.author.displayAvatarURL({ dynamic: true, format: 'png' })).setTitle(`All Commands for ${bot.user.tag}`).setFooter(`|  Help Command`, message.author.avatarURL({ dynamic: true, format: 'png', size: 2048 })).setTimestamp(Date.now())
-      message.author.send(embed).catch(err => {
-        message.reply(`Please open your DM's so that I can send you the help command! I need to do this because the command list is \`${i}\` commands long.`).then(msg => msg.delete({ timeout: 10000 }))
-        if (err == `DiscordAPIError: Cannot send messages to this user`) return;
-        console.error(err)
-      })
-      message.reply(`I sent you a DM!`).then(msg => msg.delete({ timeout: 3000 }))
+      const embed = helpEmbed.setColor('RANDOM').setThumbnail(message.author.displayAvatarURL({ dynamic: true, format: 'png' })).setTitle(`All Commands for ${bot.user.tag}`).setFooter(`|  Help Command`, message.author.avatarURL({ dynamic: true, format: 'png', size: 2048 })).setTimestamp(Date.now())
+      try {
+        await message.author.send(embed);
+        message.reply(`I sent you a DM!`).then(msg => msg.delete({ timeout: 3000 }))
+      return;
+      } catch (err) {
+        message.reply(bot.embed.setDescription(`Please open your DM's so that I can send you the help command! I need to do this because the command list is very long.`)).then(msg => msg.delete({ timeout: 10000 }))
+        return;
+      }
     }
 
     const command = bot.commands.get(args[0]) || bot.commands.find(c => c.aliases && c.aliases.includes(args[0]))
