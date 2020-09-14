e49f6efbd708fd1d8da6f56be20dcc90f8c6e244
T3g4nTheCoder <winnerkiller966@gmail.com>
2020-09-14 10:29:04 +0300

    AFKING

---
+++ commands/afk.js
@@ -0,0 +1,36 @@
+const { bot } = require('../Client');
+const { Message, Collection } = require('discord.js');
+bot.afkmap = [];
+
+module.exports = {
+    name: 'AFK',
+    description: 'Sets your AFK status!',
+    aliases: [],
+    usage: '[command] [optional reason]',
+    catagory: 'fun',
+    /**
+     * @param {Message} message
+     * @param {String[]} args
+     */
+    run: async(a, message, args) => {
+       
+
+        let reason = args.join(' ');
+        if (!reason) reason = 'No Reason Specified';
+
+        bot.afkmap.push(`${message.author.id}:${reason}`)
+
+        message.reply(bot.embed.setDescription('I set your AFK to `' + reason + '`!'));
+
+        try {
+            message.member.setNickname('[AFK] ' + message.member.nickname).catch(() => {
+                return message.reply(bot.embed.setDescription('I do not have permission to change your nickname, ' + message.author));
+            });
+        } catch (e) {
+            message.reply(bot.embed.setDescription('I do not have permission to change your nickname, ' + message.author))
+
+            if (e.toLowerCase().includes('discordapiError: missing permissions')) return;
+            console.error(e)
+        }
+    }
+}
