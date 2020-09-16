696a2303604cb8ceb4222cda37e04270a0bb0ffd
T3g4nTheCoder <winnerkiller966@gmail.com>
2020-09-16 08:52:05 +0300

    something :)

--- commands/ping.js
+++ commands/ping.js
@@ -1,29 +1,32 @@
 const { MessageEmbed } = require("discord.js")
+const { bot } = require('../Client');
+const emoji = bot.guilds.cache.get('714809218024079430').emojis.cache.find(e => e.name === 'loading')
 
- module.exports = {
+module.exports = {
     name: 'ping',
     aliases: ['p', 'pong'],
     catagory: 'fun',
     usage: '[command]',
     description: "Check your ping to the bot!",
-    async run (bot, message, args) {
-    
-    message.delete({ timeout: 0 })
-    let msg = await message.channel.send("Pinging...")
+    async run(a, message, args) {
 
-    var time = msg.createdTimestamp - message.createdTimestamp + " ms"
-    if (time.startsWith('-')) var time = message.createdTimestamp - msg.createdTimestamp + ' ms'
+        message.delete({ timeout: 0 })
+        let msg = await message.channel.send(`${emoji} Pinging...`)
 
-    const embed = new MessageEmbed()
-    .setTitle("🏓 Pong! 🏓")
-    .addField(`Bot`, `:loading: ` + time)
-    .addField("API", `:755297717558968351:` + bot.ws.ping + " ms")
-    .setColor('RANDOM')
-    .setFooter(`|   Ping Command`, message.author.avatarURL({ dynamic: true }))
-    .setTimestamp(Date.now())
+        var time = `${msg.createdTimestamp - message.createdTimestamp} MS`
+        if (time.startsWith('-')) var time = `${message.createdTimestamp - msg.createdTimestamp} MS`
 
-    
-    await msg.edit(embed)
-    msg.edit("")
+        const embed = new MessageEmbed()
+            .setTitle("🏓 Pong! 🏓")
+            .addField(`Bot`, `${emoji} ` + time)
+            .addField("API", `${emoji} ${bot.ws.ping} MS`)
+            .setColor('RANDOM')
+            .setFooter(`|   Ping Command`, message.author.avatarURL({ dynamic: true }))
+            .setTimestamp(Date.now())
 
-}}
+
+        await msg.edit(embed)
+        msg.edit("")
+
+    }
+}
