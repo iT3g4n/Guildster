bd6dc6a36bdcadabf30d2f0bd1aeef6890e7337d
T3g4nTheCoder <winnerkiller966@gmail.com>
2020-09-22 10:38:17 +0300

    a

--- commands/kick.js
+++ commands/kick.js
@@ -1,8 +1,5 @@
 const { MessageEmbed, Client, Message } = require("discord.js");
-const mongo = require("../mongo");
-const guilds = require(`../schemas/guildSchema`);
-const guildSchema = require("../schemas/guildSchema");
-const embed = require(`../newembed`)
+const {bot} = require('../index');
 
 module.exports = {
     name: 'Kick',
@@ -16,43 +13,18 @@ module.exports = {
      * @param {Message} message 
      * @param {String[]} args 
      */
-    run: async(bot, message, args) => {
-        const mention = message.mentions.users.first()
-        const id = message.guild.members.cache.get(args[0])
-        if (!id && !mention) return message.reply(`Please mention a person to kick.`).then(msg => msg.delete({ timout: 5000 }))
+    run: async(a, message, args) => {
+        
+        if (!message.member.hasPermission(`KICK_MEMBERS`)) return message.channel.send(bot.error(`You are not allowed to run that command!`)).then(m => m.delete({ timeout: 5000 }))
 
-        if (!mention) {
-            var mi = id.id
-            var mt = id.user.tag
-            var user = id
-        } else {
-            var mi = mention.id
-            var mt = mention.tag
-            var user = mention
-        }
+        const mention = message.mentions.members.first() || message.guild.members.cache.get(args[0])
+        if (!mention) return message.reply(`Please mention a person to kick.`).then(msg => msg.delete({ timeout: 5000 }))
+
 
         const reason = args.slice(1).join(' ')
         if (!reason) return message.reply(`Please give a reason.`).then(msg => msg.delete({ timout: 5000 }))
 
-        user.kick(reason);
+        mention.kick(reason);
 
-        await mongo(async mongoose => {
-            
-            try {
-                let data = await guildSchema.findOne({_id: message.guild.id})
-                if (!data.Logs) return;
-                bot.channels.cache.get(data.Logs).send(embed(message, this).then(async embed => {
-                    embed
-                        .setTitle(`${mt} has been kicked`)
-                        .setDescription(`
-                        User: ${user}
-                        Reason: ${reason}
-                        Moderator: ${message.author}
-                        `)
-                }))
-            } finally {
-                mongoose.connection.close()
-            }
-        })
     }
 }
