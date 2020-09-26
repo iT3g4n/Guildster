d1fa3bcb69f0c1a34fad9c560903b5fdb495666c
T3g4n <68778866+T3g4nTheCoder@users.noreply.github.com>
2020-09-26 17:40:05 +0300

    a

--- commands/play.js
+++ commands/play.js
@@ -20,17 +20,17 @@ this.run = async(a, message, args) => {
 
     const embed = new MessageEmbed().setColor('GREEN')
 
-    const vc = await message.member.voice.channel.join();
-    const link = await search(query).then((data, err) => {
-        if (err) console.error(err);
-
-        console.log(data.all[0])
-        vc.play(data.all[0].url)
-        embed.addField('Now Playing', data.all[0].title);
-        embed.addField('Artist', data.all[0].author.name);
-        embed.addField('Length', data.all[0].duration.timestamp);
-        embed.setURL(data.all[0].url)
-        bot.musicqueue.set(message.guild.id, `${data.all[0].title}~~${data.all[0].duration.timestamp}`);
-    });
+    await message.guild.me.voice.channel.join().then(vc => {
+        await search(query).then((data, err) => {
+            if (err) console.error(err);
+    
+            vc.play(data.all[0].url)
+            embed.addField('Now Playing', data.all[0].title);
+            embed.addField('Artist', data.all[0].author.name);
+            embed.addField('Length', data.all[0].duration.timestamp);
+            embed.setURL(data.all[0].url)
+            bot.musicqueue.set(message.guild.id, `${data.all[0].title}~~${data.all[0].duration.timestamp}`);
+        });
+    })
     message.reply(embed);
 }
