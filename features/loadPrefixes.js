a45bd4ac467bca291991c8881c312d9dbf99180c
T3g4nTheCoder <winnerkiller966@gmail.com>
2020-09-30 08:53:49 +0300

    a

--- features/loadPrefixes.js
+++ features/loadPrefixes.js
@@ -4,8 +4,8 @@ const guildSchema = require('../schemas/guildSchema');
 module.exports = async() => {
     bot.guilds.cache.forEach(async guild => {
         const c = await guildSchema.findOne({ _id: guild.id });
+        if (!c.Prefix) return;
         const b = c.Prefix;
-        if (!b) return;
         bot.prefixes.set(guild.id, b);
         console.log(`Prefix loaded for ${guild.name}`)
     });
