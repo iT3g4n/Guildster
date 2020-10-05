8f47208806fbde1d893601cd8f0ccf3eefb7636c
T3g4n <68778866+T3g4nTheCoder@users.noreply.github.com>
2020-10-05 17:46:00 +0300

    big brain time

--- features/loadPrefixes.js
+++ features/loadPrefixes.js
@@ -2,19 +2,14 @@ const { bot } = require('../index');
 const guildSchema = require('../schemas/guildSchema');
 
 module.exports = async () => {
-    await bot.guilds.cache.forEach(async guild => {
+    bot.guilds.cache.forEach(async (guild) => {
         await guild.fetch();
-        if (guild.id == '741361663298764883') return;
         const c = await guildSchema.findOne({ _id: guild.id });
-        if (!c) {
-            const asdf = await guild.channels.cache.first().createInvite()
-            console.log(`${guild.name} has no Prefix @ ${asdf}`);
-            return;
-        }
+        if (!c) return;
+        if (!c.Prefix) return;
         const b = c.Prefix;
         bot.prefixes.set(guild.id, b);
-        console.log(`Prefix loaded for ${guild.name} @ ${b}`)
+        console.log(`Prefix loaded for ${guild.name} @ ${b}`);
     });
-    console.log('All prefixes were loaded successfully.');
 
 };
