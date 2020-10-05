f6489190a31439378f81a1adbc98cf6be28480b4
T3g4n <68778866+T3g4nTheCoder@users.noreply.github.com>
2020-10-05 17:40:39 +0300

    a

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -19,12 +19,15 @@ module.exports = {
             upsert: true
         });
 
+        bot.prefixes.set(guild.id, '*');
+
         const embed = new MessageEmbed()
             .setColor('RANDOM')
             .setTitle(`Thank You for inviting me to your server! To get yourself up and running, Please do \`*help\`!`);
         console.log(`New Guild:`, guild.name);
         i = 0
         const channel = guild.channels.cache.find(channel => channel.type === 'text' && channel.permissionsFor(guild.me).has('SEND_MESSAGES'))
-        channel.send(embed)
+        channel.send(embed);
+
     }
 }
