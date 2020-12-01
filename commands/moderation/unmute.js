fe518954fa2ed74debe74cc9e522b779ed646ebd
T3g4nTheCoder <winnerkiller966@gmail.com>
2020-12-01 08:55:34 +0300

    Update unmute.js

--- commands/moderation/unmute.js
+++ commands/moderation/unmute.js
@@ -1,36 +1,92 @@
-const { Message, MessageEmbed } = require("discord.js");
-const { bot } = require('./../../index');
-const mongo = require('./../../mongo');
-const guilds = require('./../../schemas/guildSchema')
+const { Message } = require("discord.js");
+const { bot: params } = require("../../index");
+const guilds = require("../../schemas/guildSchema");
+const muteSchema = require("../../schemas/muteSchema");
+
+function send(sendchannel, mention, reason, time, message) {
+  const embed = new MessageEmbed()
+    .setTitle(`User Unmuted`)
+    .addField(`User`, `<@${mention.id}>`, true)
+    .addField(`Moderator`, `<@${message.author.id}>`, true)
+    .addField(`Reason`, `${reason}`, true)
+    .setColor(`GREEN`);
+
+  try {
+    bot.channels.cache.get(sendchannel).send(embed);
+  } catch (e) {
+    return e;
+  }
+}
+
+/**
+ * @param {params} bot
+ * @param {Message} message
+ * @param {String[]} args
+ */
+async function run(bot, message, args) {
+  function error(desc) {
+    return message.reply(bot.error(desc));
+  }
+
+  // ___________________________________________________________________________________________________________________________________________________________________________________________________________________
+
+  const { member, guild, channel } = message;
+  let reason = args.slice(1).join(" ");
+  if (!reason) reason = "No Reason Specified";
+
+  // ___________________________________________________________________________________________________________________________________________________________________________________________________________________
+
+  if (!member.hasPermission("MANAGE_ROLES"))
+    return error("You don't seem to have enough permissions!");
+
+  const mention =
+    guild.members.cache.get(args[0]) || message.mentions.members.first();
+
+  const { roleId } = await muteSchema.findOne({
+    _id: guild.id,
+    userId: mention.id,
+  });
+
+  if (!guild.roles.cache.has(roleId))
+    return error(
+      'The "Muted" role in this server does not seem to exist anymore!'
+    );
+
+  const role = guild.roles.cache.get(roleId);
+
+  if (!mention.roles.cache.has(roleId))
+    return error("This person is not muted!");
+
+  if (!role)
+    return error(
+      'The "Muted" role in this server does not seem to exist anymore!'
+    );
+
+  try {
+    await mention.roles.remove(role);
+    await muteSchema.findOneAndDelete({
+      _id: guild.id,
+      userId: mention.id,
+    });
+  } catch (e) {
+    error("I could not take that role away from " + mention.user.tag);
+    return;
+  }
+
+  bot.e(`I have unmuted <@${mention.id}> with the reason of "${reason}"`, true);
+
+  const channel = await guildSchema.findOne({ _id: message.guild.id });
+  if (!channel.Logs) return;
+  const sendchannel = channel.Logs;
+
+  send(sendchannel, mention, reason, message);
+}
 
 module.exports = {
-    name: 'Unmute',
-    description: 'Unmutes the mentioned person!',
-    aliases: ['u', 'talk'],
-    catagory: 'moderation',
-    usage: '[command] [mention or id]',
-    /**
-     * @param {Message} message
-     * @param {String[]} args
-     */
-    run: async (a, message, args) => {
-        const mention = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
-        if (!mention) return message.reply(bot.embed.setDescription('You did not mention anybody!'));
-
-        if (!mention.roles.cache.find(r => r.name === 'Muted')) return message.reply(bot.embed.setDescription('That person is not muted!'))
-
-        mention.roles.remove(mention.roles.cache.find(r => r.name === 'Muted'));
-
-        message.reply(bot.embed.setDescription(`'Succesfully unmuted <@${mention.id}>!`));
-
-        const embed = new MessageEmbed()
-            .setTitle(`User Unmuted`)
-            .addField(`User`, `<@${mi}>`, true)
-            .addField(`Moderator`, `<@${message.author.id}>`, true)
-            .setColor(`GREY`)
-
-            const result = await guilds.findOne({ _id: message.guild.id })
-            if (!result) return;
-            bot.channels.cache.get(result.Logs).send(embed)
-    },
-}
+  name: "Unmute",
+  description: "Unmutes the mentioned person!",
+  aliases: ["u", "talk"],
+  catagory: "moderation",
+  usage: "[command] [mention or id]",
+  run,
+};
