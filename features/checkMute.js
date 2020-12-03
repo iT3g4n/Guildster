4001d41f7d154eb8ea936c630de074311325c15c
T3g4nTheCoder <winnerkiller966@gmail.com>
2020-12-03 12:11:31 +0300

    Update checkMute.js

--- features/checkMute.js
+++ features/checkMute.js
@@ -1,36 +1,40 @@
-const { bot } = require('../index');
-const guildSchema = require('../schemas/guildSchema');
-const muteSchema = require('../schemas/muteSchema');
+const { bot } = require("../index");
+const guildSchema = require("../schemas/guildSchema");
+const muteSchema = require("../schemas/muteSchema");
 
 module.exports = () => {
+  bot.guilds.cache.forEach((guild) => {
+    setInterval(async () => {
+      let date = Date.now();
 
-    bot.guilds.cache.forEach(guild => {
-        setInterval(async () => {
-            let date = Date.now();
+      const results = await muteSchema.find({ _id: guild.id });
 
-            const results = await muteSchema.find({ _id: guild.id });
+      if (!results) return;
 
-            if (!results) return;
+      results.forEach(async (result) => {
+        if (result.expires < date == false) return;
 
-            results.forEach(result => {
-                if ((result.expires < date) == false) return;
+        const member = guild.members.cache.get(result.userId);
+        const doc = await muteSchema.findOne({
+          _id: guild.id,
+          uswerId: member.id,
+        });
 
-                const member = guild.members.cache.get(result.userId);
-                const doc = await muteSchema.findOne({ _id: guild.id, uswerId: member.id });
+        let role = guild.roles.cache.get(result.roleId);
+        if (!role) {
+          doc.deleteOne();
+          return;
+        }
 
-                let role = guild.roles.cache.get(result.roleId);
-                if (!role) {
-                    doc.deleteOne()
-                    return;
-                }
-                
-                if (!member.roles) {
-                    doc.deleteOne()
-                    return;
-                }
-                member.roles.remove(role, 'Mute time up').catch(e => console.error(e));
-                doc.deleteOne()
-            });
-        }, 1000 * 5)
-    });
+        if (!member.roles) {
+          doc.deleteOne();
+          return;
+        }
+        member.roles
+          .remove(role, "Mute time up")
+          .catch((e) => console.error(e));
+        doc.deleteOne();
+      });
+    }, 1000 * 5);
+  });
 };
