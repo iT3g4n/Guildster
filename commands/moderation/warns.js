17ac6d6eee01921467288cc25aa06e9b8e692cbd
T3g4nTheCoder <winnerkiller966@gmail.com>
2020-12-03 10:27:15 +0300

    Update warns.js

--- commands/moderation/warns.js
+++ commands/moderation/warns.js
@@ -1,5 +1,5 @@
 const { Client, Message, MessageEmbed } = require("discord.js");
-const warns = require(`./../../schemas/warnSchema`);
+const warns = require(`../../schemas/warnSchema`);
 
 module.exports = {
   name: "Warns",
@@ -29,8 +29,8 @@ module.exports = {
         mention.user.displayAvatarURL({ dynamic: true })
       );
 
-    const msg = await message.channel.send(
-      `Getting warnings for ${mention.user.tag}...`
+    const msg = await message.channel.send( 
+      bot.embed.setDescription(`Getting warnings for ${mention.user.tag}...`)
     );
 
     const results = await warns.findOne({
@@ -52,9 +52,6 @@ module.exports = {
       );
     }
 
-    msg.delete().catch((e) => {
-      console.error(e);
-    });
-    message.channel.send(embed);
+    msg.edit(embed);
   },
 };
